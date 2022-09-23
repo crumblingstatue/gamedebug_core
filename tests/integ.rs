50f98206e5a01d3cb18fa4ffb074df64d0b409bd
crumblingstatue <radiantstatue@gmail.com>
2022-09-23 18:40:48 +0200

    Add integration test

---
+++ tests/integ.rs
@@ -0,0 +1,11 @@
+use gamedebug_core::{imm, imm_dbg, per, per_dbg};
+
+#[test]
+fn test() {
+    gamedebug_core::toggle();
+    dbg!(gamedebug_core::enabled());
+    per!("Hi!");
+    imm!("Hi!");
+    per_dbg!(42);
+    imm_dbg!(42);
+}
